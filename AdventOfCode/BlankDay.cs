3e0201e9671f69f1b53e13fbad28f7c88702d886
Will <me@wkaye.dev>
2022-12-30 12:23:01 +0000

    Create BlankDay.cs

---
+++ AdventOfCode/BlankDay.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode;
+
+public class BlankDay : BaseDay
+{
+    private readonly string[] _input;
+    
+    public BlankDay()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        return new ValueTask<string>("");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new ValueTask<string>("");
+    }
+}
